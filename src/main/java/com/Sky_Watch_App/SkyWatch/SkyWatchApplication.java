755678fa249afbbf13b707d324eda3b05e580e97
Victoria Giler <104544006+victoriagiler@users.noreply.github.com>
2023-07-11 15:07:17 -0400

    Add files via upload

---
+++ src/main/java/com/Sky_Watch_App/SkyWatch/SkyWatchApplication.java
@@ -0,0 +1,13 @@
+package com.Sky_Watch_App.SkyWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sky_Watch_App/SkyWatch/SkyWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sky_Watch_App.SkyWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
