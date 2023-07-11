4b0b992827b1c26af4c55701b2fc782ebe9287fb
shreyab <shreya.bakharel@gmail.com>
2023-07-11 13:13:21 -0700

    added packages and finished FlightController

---
+++ src/main/java/com/Sky_Watch_App/SkyWatch/entities/Flight.java
@@ -0,0 +1,4 @@
+package com.Sky_Watch_App.SkyWatch.entities;
+
+public class Flight {
+}
---
+++ src/main/java/com/Sky_Watch_App/SkyWatch/repositories/FlightRepository.java
@@ -0,0 +1,8 @@
+package com.Sky_Watch_App.SkyWatch.repositories;
+
+import com.Sky_Watch_App.SkyWatch.entities.Flight;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FlightRepository extends CrudRepository <Flight, String>{
+
+}
