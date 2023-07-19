5bcc13c3ad27db75ad37d485360ea5772a60ec2e
Victoria <victoriaashleygiler@gmail.com>
2023-07-19 10:30:20 -0400

    deleted dependencies

--- src/main/java/com/Sky_Watch_App/SkyWatch/controllers/UserController.java
+++ src/main/java/com/Sky_Watch_App/SkyWatch/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "*")
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
